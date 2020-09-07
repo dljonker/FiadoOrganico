c5edf84b1cee213bac8dc56672abced6c2401534
Davy Jonker <62754869+dljonker@users.noreply.github.com>
2020-09-07 11:17:27 -0300

    Package renomeado

--- src/fiado/Consumidor.java
+++ src/fiado/Consumidor.java
@@ -1,7 +1,8 @@
-
+package fiado;
 public class Consumidor {
 
 	// Atributo
+	//PACKAGE - PUBLIC - PROTECTED - PRIVATE
 	String nome;
 	int[] fiados;
 }
--- src/fiado/Main.java
+++ src/fiado/Main.java
@@ -1,4 +1,4 @@
-
+package fiado;
 
 import java.util.Scanner;
 
